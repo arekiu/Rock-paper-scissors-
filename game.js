c51d0e2ca6cbdcfa2ab4391ce1aed2b7c6979c8f
Arekiu <alex.isaguilera@hotmail.com>
2022-05-31 17:02:06 +0200

    I created a loop function so the game repeats until 5 rounds. it also says who is the winner in every round and the final winner at the end

--- game.js
+++ game.js
@@ -1,27 +1,32 @@
 const options =  ['rock','paper','scissors'];
 
 let computerSelection = '';
+let playerSelection = '';
 
-function computerPlay() { 
-    computerSelection = options[Math.floor(Math.random()* options.length)];
-    return computerSelection;
 
-}
+let human = 0;
+let computer = 0;
 
-computerPlay();
+
+
+function round(number) {
+
+    function computerPlay() { 
+        computerSelection = options[Math.floor(Math.random()* options.length)];
+        return computerSelection;
     
+    }
 
-let playerSelection = (prompt('Choose rock, paper or scissors:')).toLowerCase();
-
-console.log(`You have chosen ${playerSelection} and the computer have chosen ${computerSelection}`);
-
-function round(computerSelection,playerSelection) {
+    computerPlay();
+    playerSelection = (prompt('Choose rock, paper or scissors:')).toLowerCase();
+    console.log(`You have chosen ${playerSelection} and the computer have chosen ${computerSelection}`);
 
     if ((computerSelection === 'rock' && playerSelection === 'paper') ||
         (computerSelection === 'paper' && playerSelection === 'scissors') ||
         (computerSelection === 'scissors' && playerSelection === 'rock')
     ) {
         console.log('You won!');
+        human += 1;
 
     }
     else if(computerSelection === playerSelection) {
@@ -29,7 +34,29 @@ function round(computerSelection,playerSelection) {
     }
     else {
         console.log('You lose...')
+        computer += 1;
+    }
+    console.log(`The score is >>> Human:${human} VS Computer:${computer}`);
+} 
+
+
+    
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        
+        round(i);   
+     }
+    if (human>computer) {
+        console.log('You have defeated the stupid computer!');
+    }
+    else if (human===computer) {
+        console.log('None won, none lose, evereybody is happy!');
+    }
+    else {
+        console.log('The computer is better than you...');
     }
 } 
 
-round(computerSelection,playerSelection);
+game(); 
